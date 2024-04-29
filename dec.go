e42874c7906560f47adaee6625064341085c8f69
Casey Haakenson <millisecond@gmail.com>
2024-04-29 16:31:31 -0600

    Try with a -l

--- dec.go
+++ dec.go
@@ -24,7 +24,7 @@ package fdkaac
 
 /*
 #cgo CFLAGS: -I./fdk-aac/libAACdec/include/ -I./fdk-aac/libSYS/include/
-#cgo LDFLAGS: -L./fdk-aac/libs/ -lm
+#cgo LDFLAGS: -L./fdk-aac/libs/ -lm -lfdk-aac
 #include "aacdecoder_lib.h"
 
 typedef struct {
